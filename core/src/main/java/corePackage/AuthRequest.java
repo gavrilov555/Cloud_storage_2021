2e82ca7a8291beca8ee16d2d7a2be16ee134081e
gavrilov_v <gavrilov_v92@mail.ru>
2021-10-14 01:36:23 +0500

    lesson 6 (in progress...)

---
+++ core/src/main/java/corePackage/AuthRequest.java
@@ -0,0 +1,25 @@
+package corePackage;
+
+public class AuthRequest  extends  AbstractCommand{
+
+    public AuthRequest(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    private String login;
+    private String password;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public CommandType getType() {
+        return CommandType.AUTH_REQUEST;
+    }
+}
